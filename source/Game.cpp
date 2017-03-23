5e05e54f9867760a636183e0d2f0c8f3576d3613
Henry Hirsch <henry@w3-net.de>
2017-03-23 20:03:01 +0100

    Add minimalistic game loop

--- source/Game.cpp
+++ source/Game.cpp
@@ -1,5 +1,6 @@
 #include <wonder/Game.hpp>
-
+#include <string>
+#include <linenoise.hpp>
 #include <iostream>
 
 using namespace radix;
@@ -7,6 +8,67 @@ using namespace radix;
 namespace wonder {
 
 Game::Game() {
+  const auto path = "history.txt";
+
+  // Enable the multi-line mode
+  linenoise::SetMultiLine(true);
+
+  // Set max length of the history
+  linenoise::SetHistoryMaxLen(4);
+
+  linenoise::SetCompletionCallback([](const char* editBuffer, std::vector<std::string>& completions) {
+      if (editBuffer[0] == 'h') {
+        completions.push_back("help");
+      }
+
+      if (editBuffer[0] == 'e') {
+        completions.push_back("exit");
+        completions.push_back("echo");
+      }
+
+      if (editBuffer[0] == 'q') {
+        completions.push_back("quit");
+      }
+    });
+
+  // Load history
+  linenoise::LoadHistory(path);
+
+  while (true) {
+      std::string line;
+
+      auto quit = linenoise::Readline("command > ", line);
+
+      if (quit) {
+        break;
+      }
+
+      if (line == "help") {
+        std::cout <<  "Valid commands:" << std::endl;
+        std::cout <<  "quit, run, exit, help" << std::endl;
+      }
+
+      if (line == "quit") {
+        std::cout <<  "Closing console" << std::endl;
+        break;
+      }
+
+      if (line == "exit") {
+        std::cout <<  "Exiting game" << std::endl;
+        exit(0);
+      }
+
+      if (line == "echo") {
+        std::cout <<  line << std::endl;
+      }
+
+      // Add line to history
+      linenoise::AddHistory(line.c_str());
+
+      // Save history
+      linenoise::SaveHistory(path);
+  }
+
   std::cout << "Hope you enjoyed the game.";
 }
 
